508c2fd8f97b2d3f7c1f0544bbff3178cac26154
Sergej Belko <Belko936@gmail.com>
2021-05-03 22:36:05 +0300

    - Add Bottom Navigation

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,15 +2,29 @@ package com.example.weatherapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.weatherapp.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        setupBottomNavigation()
     }
 
+    private fun setupBottomNavigation() {
+        val navController = findNavController(R.id.nav_host_fragment)
+        binding.bottomNavigationView.setOnNavigationItemReselectedListener {
+            // Empty Block -> Do not write any code here
+        }
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
 }
