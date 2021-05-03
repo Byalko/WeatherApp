620309fc5fd53c140444c86680dae0d41a777a86
Sergej Belko <Belko936@gmail.com>
2021-05-03 19:01:24 +0300

    - Improve location permission with Easy Permission Library, get last known location with Fused location provider

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,41 +1,86 @@
 package com.example.weatherapp
 
 import android.Manifest
-import android.content.pm.PackageManager
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.vmadalin.easypermissions.EasyPermissions
+import com.vmadalin.easypermissions.dialogs.SettingsDialog
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
+
+    companion object {
+        const val PERMISSION_LOCATION_REQUEST_CODE = 1
+    }
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.btn_location)
-        button.setOnClickListener {
-            requestPermission()
-            if (hasLocationPermissionGranted()){
-                Toast.makeText(this,"Permission granted",Toast.LENGTH_LONG).show()
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        val button2 = findViewById<Button>(R.id.btn_location)
+        requestLocationPermission()
+        Log.d("Location", "OnCreate")
+
+        button2.setOnClickListener {
+            fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+                if (location != null) {
+                    Log.d("Location", location.latitude.toString())
+                    Log.d("Location", location.longitude.toString())
+                } else {
+                    Log.d("Location", "Error")
+                }
             }
         }
-
     }
 
-    private fun hasLocationPermissionGranted() =
-            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) ==
-                    PackageManager.PERMISSION_GRANTED
+    private fun hasLocationPermission() =
+        EasyPermissions.hasPermissions(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
 
-
-    private fun requestPermission() {
-        val permissionToRequest = mutableListOf<String>()
-        if(!hasLocationPermissionGranted()) {
-            permissionToRequest.add(Manifest.permission.ACCESS_COARSE_LOCATION)
-        }
-
-        if(permissionToRequest.isNotEmpty()) {
-            ActivityCompat.requestPermissions(this, permissionToRequest.toTypedArray(), 0)
+    private fun requestLocationPermission() {
+        if (hasLocationPermission()) {
+            return
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "This application cannot work without Location Permission.",
+                PERMISSION_LOCATION_REQUEST_CODE,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
         }
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms.first())) {
+            SettingsDialog.Builder(this).build().show()
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {}
+
 }
