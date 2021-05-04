2572647fb82178a368d2daf36026c595cf6e95cb
Sergej Belko <Belko936@gmail.com>
2021-05-05 00:36:26 +0300

    - add Result class, create repository + get data from api

---
+++ app/src/main/java/com/example/weatherapp/repository/WeatherRepository.kt
@@ -0,0 +1,27 @@
+package com.example.weatherapp.repository
+
+import com.example.test_diplom.util.Resource
+import com.example.weatherapp.data.WeatherAPI
+import com.example.weatherapp.data.model.WeatherModel
+import retrofit2.Response
+import javax.inject.Inject
+
+class WeatherRepository @Inject constructor(
+    private val api: WeatherAPI
+) {
+
+    suspend fun getFiveWeather(lat:Double,lon:Double) : Resource<WeatherModel> = checkHttpResponse(api.getFiveDay(lat,lon))
+
+    private fun <T> checkHttpResponse(response: Response<T>): Resource<T> {
+        return try {
+            val result = response.body()
+            if (response.isSuccessful && result != null) {
+                Resource.Success(result)
+            } else {
+                Resource.Error(response.message())
+            }
+        } catch (e: Exception) {
+            Resource.Error(e.message.toString())
+        }
+    }
+}
