2d22dc24902ea1f8770c9dae9b37f8ee1d15979c
Sergej Belko <Belko936@gmail.com>
2021-05-11 13:01:44 +0300

    - add permission + request to update location (getLastLocation)

--- app/src/main/java/com/example/weatherapp/ui/today/TodayViewModel.kt
+++ app/src/main/java/com/example/weatherapp/ui/today/TodayViewModel.kt
@@ -1,7 +1,21 @@
 package com.example.weatherapp.ui.today
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.weatherapp.repository.WeatherRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class TodayViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class TodayViewModel @Inject constructor(
+    private val weatherRepository: WeatherRepository
+) : ViewModel() {
+
+    fun getData(lat:Double,lon:Double) {
+
+        viewModelScope.launch {
+            weatherRepository.getFiveWeather(lat, lon)
+        }
+    }
 }
